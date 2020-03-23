2abd871a9ea0311827b3cfc1fecd91053fbe3d5a
ManjulaChandirasekaran <35280800+ManjulaChandirasekaran@users.noreply.github.com>
2020-03-23 17:12:42 -0400

    Add files via upload

---
+++ MiniProject#Two/script.js
@@ -0,0 +1,25 @@
+//Global area
+window.onload=function() {
+//Try and use classname to get a group of divs instead of getting them one by one by id
+//if you are writing the same type of code multiple times, you are doing it incorrect
+
+//Attach events using eventlistener here
+
+}
+
+/************** Function area *******************/
+
+//function provided. Add parameter(s) if needed
+function userTurn()
+{
+    //Add code here
+
+}
+
+//function provided. Add parameter(s) if needed
+function computerTurn()
+{
+    //Add code here
+
+}
+

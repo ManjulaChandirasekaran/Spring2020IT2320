c50a84c4ae6b09874996b5e780dcfb766c6eff80
ManjulaChandirasekaran <35280800+ManjulaChandirasekaran@users.noreply.github.com>
2020-04-28 14:36:04 -0400

    Update home.js

--- Lab14/home.js
+++ Lab14/home.js
@@ -47,5 +47,34 @@ $(function(){
           });
     });
 
+     $("#button3").on("click", function(){        
+        /* In this, we will use the brewery's search functionality to find 
+        a list of breweries by the search term
+        Example: https://api.openbrewerydb.org/breweries/search?query=dog
+        
+        Base URL = "https://api.openbrewerydb.org/breweries/search?query="
+        Get the "search term" from the input with id="searchTerm"
+        Append it to the Base URL to get the url
+        Then call the ajax method with the url
+        Call the api's search method using the ajax method        
+        You will get a list of breweries
+        one by one, attach them to #results div
+        */        
+    });
+
+    $("#button4").on("click", function(){
+        /* In this, we will use the search functionality of the breweries api
+        Example: URL should look like this where we pass the Id of the brewery to find it
+        https://api.openbrewerydb.org/breweries/2
+
+        Base url is "https://api.openbrewerydb.org/breweries/"
+        
+        Get the ID of the brewery from the input with id = "breweryId"
+         Append it to the Base URL to get the url
+        Then call the ajax method with the url
+        The result will be just one brewery
+        Get the brewery's id, brewery_type and name and show in the #results div
+        */
+    });
     
 });

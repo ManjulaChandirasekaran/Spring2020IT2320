b46046006c8d0329908c4c1ef1c2ba802996096c
ManjulaChandirasekaran <35280800+ManjulaChandirasekaran@users.noreply.github.com>
2021-03-17 13:06:34 -0400

    Add files via upload

---
+++ Lab7b/jslab7.js
@@ -0,0 +1,8 @@
+/*********************************************************************/
+//Global Area
+
+
+
+
+/***************** Function definition area *************/
+//write a function to change the size of the box (css)
